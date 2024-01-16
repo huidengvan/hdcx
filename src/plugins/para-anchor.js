4fc09bd8adb89a26c4f001dcbbd5ed2909cd7abe
nanqic <nanqic@163.com>
2024-01-16 09:07:46 +0800

    uncomment para anchor

--- src/plugins/para-anchor.js
+++ src/plugins/para-anchor.js
@@ -2,21 +2,21 @@ import { visit } from 'unist-util-visit'
 
 export default function paraAnchor() {
     return (tree) => {
-        // let line = 1;
+        let line = 1;
 
-        // visit(tree, 'element', (node) => {
+        visit(tree, 'element', (node) => {
 
-        //     if (node.tagName == 'p') {
-        //         const alink = {
-        //             type: 'element',
-        //             tagName: 'a',
-        //             properties: { href: `#p${line}`, name: `p${line}` },
-        //             children: [{ type: 'text', value: `[p${line}]` }],
-        //         }
+            if (node.tagName == 'p') {
+                const alink = {
+                    type: 'element',
+                    tagName: 'a',
+                    properties: { href: `#p${line}`, name: `p${line}` },
+                    children: [{ type: 'text', value: `[p${line}]` }],
+                }
 
-        //         node.children = [alink, ...node.children]
-        //         line++
-        //     }
-        // });
+                node.children = [alink, ...node.children]
+                line++
+            }
+        });
     }
 }
