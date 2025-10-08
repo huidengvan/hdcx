928376c0997e48239e39e55f7319b32c12ba006b
Yuan Hang <zuokun@gmail.com>
2025-10-07 18:37:59 -0700

    pandoc pdf gen script updated

--- bin/2pdf.sh
+++ bin/2pdf.sh
@@ -1,7 +1,7 @@
 echo "--- `date` $1 ---"
 pandoc $1 -s -f markdown \
     -V 'CJKmainfont:Yuanti SC' \
-    --pdf-engine=xelatex --toc --number-section  \
+    --pdf-engine=xelatex --toc --toc-depth=6 --tab-stop=2 --number-section  \
     -H $(dirname $0)/pdf-options.sty \
     --lua-filter $(dirname $0)/count-para.lua \
     -o $1.pdf
